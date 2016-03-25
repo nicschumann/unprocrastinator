8041c0915d443f95d606c84215be3edc8fa0359c
Martha Edwards <mke1995@gmail.com>
2016-03-24 20:59:35 -0400

    Stencil methods in client.js

---
+++ client.js
@@ -0,0 +1,41 @@
+var exports = module.exports = {};
+
+exports.get_user = function(username, password) {
+
+}
+
+exports.add_user = function(username, password) {
+
+}
+
+exports.delete_user = function(userID) {
+
+}
+
+exports.get_tasks = function(userID, sessionID) {
+
+}
+
+exports.get_task = function(userID, sessionID, taskID) {
+
+}
+
+exports.get_tasks_by_tag = function(userID, sessionID, tagID) {
+
+}
+
+exports.add_task = function(userID, sessionID, task) {
+
+}
+
+exports.delete_task = function(userID, sessionID, taskID) {
+
+}
+
+exports.edit_task = function(userID, sessionID, taskID, taskObject) {
+
+}
+
+exports.add_tag_to_task = function(userID, sessionID, taskID, tag) {
+
+}
